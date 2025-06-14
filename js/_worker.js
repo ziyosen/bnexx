1b2961b81d797f3de8e4d5b02223dfe2a4a8923e
ziyosen <lozystore@outlook.co.id>
2025-06-15 05:48:01 +0700

    Update _worker.js

--- js/_worker.js
+++ js/_worker.js
@@ -257,10 +257,6 @@ async function handleSubRequest(hostnem) {
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
-        
-
-  <!DOCTYPE html>
-  <html lang="en">
   <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -1019,11 +1015,9 @@ async function handleSubRequest(hostnem) {
         console.error(error);
     }
 };
-
-
-                    // Open link functionality
-                    openLinkBtn.onclick = () => {
-                        window.open(generatedLink, '_blank');
+          // Open link functionality
+	openLinkBtn.onclick = () => {
+		window.open(generatedLink, '_blank');
                     };
 
                 } catch (error) {
